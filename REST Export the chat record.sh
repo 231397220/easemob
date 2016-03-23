5f6a5f2ddacb843ce3a54f43fbbd3a58fd887f5e
Sam <231397220@qq.com>
2016-03-23 19:15:52 +0800

    finished

--- REST Export the chat record.sh
+++ REST Export the chat record.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
+#Using the REST command fetch chat log
+#sam@easemob.com
 
-LOG_USER=zdou
-LOG_PW=123456
-LOG_TOKEN='curl -X POST -i  "http://api.corp.gome.com.cn/management/token" -d '{"grant_type":"password","username":"$LOG_USER","password":"$LOG_PW"}''
+
+export LOG_USER=webmaster
+export LOG_PW=xiaoheiwu
+export APPKEY=easemob-demo/chatdemoui
+export REST_SER=http://172.17.16.201:8080
+export LOG_TOKEN=$(curl -X POST -i  "$REST_SER/management/token" -d '{"grant_type":"password","username":"'$LOG_USER'","password":"'$LOG_PW'"}' | grep access_token | cut -d '"' -f 4)
+
+num=1
+YESTERDAY=$(date +%Y-%m-%d --date="-1 day")
+TODAY=$(date +%Y-%m-%d)
+YES_TIME=$(date -d "$YESTERDAY 0:00:00" +%s)
+TOD_TIME=$(date -d "$TODAY 0:00:00" +%s)
+
+
+function check_message() {
+    CURSOR=$1
+    num=$2
+    curl -X GET -i -H "Authorization: Bearer $LOG_TOKEN" "$REST_SER/$APPKEY/chatmessages?ql=select+*+where+timestamp>$YES_TIME&limit=1000"&cursor=$CURSOR > message_${num}.bck
+
+   CURSOR=$(tail -n 2 message_${num}.bck |grep count|cut -d ':' -f 2)
+   echo "Finishded ${num}"
+
+   if [ "$CURSOR" -eq 0 ]; then
+     echo "Finished"
+     exit 0
+   else
+     let num++
+     check_message $CURSOR $num
+  fi
+}
+
+
+
+curl -X GET -i -H "Authorization: Bearer $LOG_TOKEN" "$REST_SER/$APPKEY/chatmessages?ql=select+*+where+timestamp>$YES_TIME&limit=1000" > message_${num}.bck
+
+CURSOR=$(tail -n 2 message_${num}.bck |grep cursor|cut -d ':' -f 2)
+echo "Finished ${num}"
+
+if [ "$CURSOR" -eq 0 ]; then
+  echo "Finished"
+  exit 0
+else
+   let num++
+   check_message $CURSOR $num
+fi
